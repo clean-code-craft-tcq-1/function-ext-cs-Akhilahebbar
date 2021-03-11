3ad3363a860b4074891f9922318002de6daf5b8c
Akhilahebbar <40620412+Akhilahebbar@users.noreply.github.com>
2021-03-11 11:57:15 +0530

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace BatteryManagement
 {
-   public static class Constants
+    public  class Warnings
+    {
+      public static string Discharge { get;set; }
+      public static string ChargePeak { get; set; }
+       public Warnings(string discharge,string chargePeak)
+        {
+            Discharge = discharge;
+            ChargePeak = chargePeak;
+        }
+    }
+    public enum BatteryStates
+    {
+        Low,
+        Normal,
+        High
+    }
+    public static class Constants
     {
         public static float minChargeRate = 0.3f;
         public static float maxChargeRate = 0.8f;
@@ -15,11 +31,6 @@ namespace BatteryManagement
         public static float thresholdPercent = 5;
         public static string LowDischarge = "Approaching Discharge";
         public static string PeakDischarge = "Approaching charge-peak";
-        public  enum BatteryStates
-        {
-            Low,
-            Normal,
-            High
-        }
+       
     }
 }
