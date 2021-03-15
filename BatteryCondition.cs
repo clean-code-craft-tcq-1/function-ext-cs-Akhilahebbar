7e0e459aa9a22b0257ed26d2ac14a3053c8d599d
Akhilahebbar <40620412+Akhilahebbar@users.noreply.github.com>
2021-03-15 22:03:22 +0530

    Update BatteryCondition.cs

--- BatteryCondition.cs
+++ BatteryCondition.cs
@@ -5,7 +5,21 @@ namespace BatteryManagement
     public static class BatteryCondition
     {
       
-      
+        public static BatteryStates CheckBatteryCondition(float currentValue, float minValue, float maxValue)
+        {
+            if (CheckMaxCondition(currentValue, maxValue) == BatteryStates.High)
+            {
+                return BatteryStates.High;
+            }
+
+            if (CheckMinCondition(currentValue, minValue) == BatteryStates.Low)
+            {
+                return BatteryStates.Low;
+
+            }
+
+            return BatteryStates.Normal;
+        }
 
         private static BatteryStates CheckMaxCondition(float actualValue, float maxValue)
         {
