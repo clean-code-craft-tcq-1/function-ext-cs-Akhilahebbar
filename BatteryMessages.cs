7c0849532e272cf783bc6c1dd55bded45c97ed6e
Akhilahebbar <40620412+Akhilahebbar@users.noreply.github.com>
2021-03-18 20:13:18 +0530

    Update BatteryMessages.cs

--- BatteryMessages.cs
+++ BatteryMessages.cs
@@ -6,26 +6,19 @@ namespace BatteryManagement
 {
    public static class BatteryMessages
     {
-        public static bool isGerman = false;
         public static void DisplayBatteryStatus(string property, BatteryStates batteryLevel)
         {
-            if (isGerman)
-            {
-                Console.WriteLine($"Batterie {property} ist {batteryLevel}");
-                return;
-            }
+           
             Console.WriteLine($"Battery {property} is {batteryLevel}");
         }
 
-        public static void DisplayWarningMessage(string warning)
+        public static void DisplayWarningMessage(string warning,IReporter reporter)
         {
-            if (isGerman)
-            {
-                Console.WriteLine($"{warning}");
-                return;
-            }
-
-            Console.WriteLine($"{warning}");
+            Console.WriteLine($"Console Output:{warning}");
+            reporter = new TextReporter();
+            reporter.ReportData(warning);
+            reporter = new UIReporter();
+            reporter.ReportData(warning);
         }
     }
 }
