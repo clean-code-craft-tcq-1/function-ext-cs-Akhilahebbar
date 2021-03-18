d7ceb34e8a047cc32d7e675156fb782e2d95640d
Akhilahebbar <40620412+Akhilahebbar@users.noreply.github.com>
2021-03-18 20:11:58 +0530

    Update TestBatteryStatus.cs

--- TestBatteryStatus.cs
+++ TestBatteryStatus.cs
@@ -2,29 +2,30 @@ using System;
 
 namespace BatteryManagement
 {
-    internal class TestBatteryStatus
+    internal  class TestBatteryStatus
     {
-       
-        public static void CheckBatteryStatus(float temperature, float soc, float chargeRate)
+      
+
+        public  void CheckBatteryStatus(float temperature, float soc, float chargeRate)
         {
           CheckTemperature(temperature);
             CheckStateOfCharge(soc);
             CheckChargeRate(chargeRate);
         }
-        public static void CheckTemperature(float temperature)
+        public  void CheckTemperature(float temperature)
         {
             CheckBatteryLevel(temperature, Constants.minTemperature, Constants.maxTemperature);
             BatteryStates batteryTemperature = BatteryCondition.CheckBatteryCondition(temperature, Constants.minTemperature, Constants.maxTemperature);
             BatteryMessages.DisplayBatteryStatus("Temperature", batteryTemperature);
         }
-        public static void CheckStateOfCharge(float soc)
+        public  void CheckStateOfCharge(float soc)
         {
             CheckBatteryLevel(soc, Constants.minStateOfCharge, Constants.maxStateOfCharge);
             BatteryStates batteryChargeState = BatteryCondition.CheckBatteryCondition(soc, Constants.minStateOfCharge, Constants.maxStateOfCharge);
             BatteryMessages.DisplayBatteryStatus("State of charge", batteryChargeState);
         }
 
-        public static void CheckChargeRate(float chargeRate)
+        public  void CheckChargeRate(float chargeRate)
         {
             CheckBatteryLevel(chargeRate, Constants.minChargeRate, Constants.maxChargeRate);
             BatteryStates batteryChargeRate = BatteryCondition.CheckBatteryCondition(chargeRate, Constants.minChargeRate, Constants.maxChargeRate);
@@ -34,14 +35,15 @@ namespace BatteryManagement
       
 
        
-        public static void CheckBatteryLevel(float ActualValue, float minValue, float maxValue)
+        public  void CheckBatteryLevel(float ActualValue, float minValue, float maxValue)
         {
+            BatteryBreachLevel breachlevel = new BatteryBreachLevel();
             float Thresholdlimit = CalculateThreshold(ActualValue, Constants.thresholdPercent);
-            BatteryBreachLevel.CheckMinBreachLevel(Thresholdlimit, ActualValue, minValue);
-            BatteryBreachLevel.CheckMaxBreachLevel(Thresholdlimit, ActualValue, maxValue);
+            breachlevel.CheckMinBreachLevel(Thresholdlimit, ActualValue, minValue);
+            breachlevel.CheckMaxBreachLevel(Thresholdlimit, ActualValue, maxValue);
         }
 
-        public static float CalculateThreshold(float actualValue, float thresholdPercent)
+        public  float CalculateThreshold(float actualValue, float thresholdPercent)
         {
             return thresholdPercent * actualValue / 100;
         }
